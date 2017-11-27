9c61c8ad0c62df172052f0aa217a6d2def81c103
sozl923 <sozl923@ASN2808.anadolusigorta.pvt>
2017-11-27 17:17:46 +0300

    angular

---
+++ src/main/resources/js/loginController.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+(function(){
+	'use strict';
+	
+	angular
+		.module('app')
+		.controller('LoginController', loginController);
+	
+	LoginController.$inject = [];
+	
+	function LoginController(){
+		var vm = this;
+	}
+})();
