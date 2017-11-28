f23cb597aa0396c4ac421ce60917cd96c0ff166b
sozl923 <sozl923@ASN2808.anadolusigorta.pvt>
2017-11-28 20:56:41 +0300

    .

--- src/main/java/com/mytodolist/Controller/ViewController.java
+++ src/main/java/com/mytodolist/Controller/ViewController.java
@@ -1,19 +1,26 @@
 package com.mytodolist.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ViewController {
+
+	@Autowired
+	public ViewController() {
+		// TODO Auto-generated constructor stub
+	}
 
 	@RequestMapping("/")
 	public String index() {
-		return "index";
+		return "../index";
 	}
-	
+
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 }
