a178c1b75d581c4d69dbb4d191d086c9041f4b2f
sozl923 <sozl923@ASN2808.anadolusigorta.pvt>
2017-11-29 10:42:49 +0300

    changes for frontend part

--- src/main/java/com/mytodolist/Controller/ViewController.java
+++ src/main/java/com/mytodolist/Controller/ViewController.java
@@ -3,24 +3,23 @@ package com.mytodolist.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ViewController {
 
 	@Autowired
 	public ViewController() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	@RequestMapping("/")
 	public String index() {
-		return "../index";
+		return "index";
 	}
-
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
-
+	
 }
--- src/main/java/com/mytodolist/MytodolistApplication.java
+++ src/main/java/com/mytodolist/MytodolistApplication.java
@@ -2,8 +2,10 @@ package com.mytodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"com.mytodolist.Controller"})
 public class MytodolistApplication {
 
 	public static void main(String[] args) {
